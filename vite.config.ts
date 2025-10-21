81c39f8a364399ea71aefc1be7ce915302244d48
BernardSantosa <bernardsantosa50@gmail.com>
2025-10-21 22:04:26 +0700

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  // base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
+  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
 })
